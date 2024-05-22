2941566adb7ce1c24fad76e070a986004132ec81
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-05-21 22:19:09 -0500

    added object functions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { chunkArray, findUniqueElements } = require('./modules/arrayUtils');
+const { deepCloneObject, mergeObjects } = require('./modules/objectUtils');
 
 // Problem 1: Chunking an Array
 const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -10,12 +11,12 @@ const array2 = [1, 2, 2, 3, 4, 4, 5];
 const uniqueArray = findUniqueElements(array2);
 console.log('Unique Array:', uniqueArray); // [1, 2, 3, 4, 5]
 
-// // Problem 3: Deep Cloning an Object
-// const object1 = { a: 1, b: { c: 2 } };
-// const deepClonedObject = deepCloneObject(object1);
-// console.log('Deep Cloned Object:', deepClonedObject); // { a: 1, b: { c: 2 } }
+// Problem 3: Deep Cloning an Object
+const object1 = { a: 1, b: { c: 2 } };
+const deepClonedObject = deepCloneObject(object1);
+console.log('Deep Cloned Object:', deepClonedObject); // { a: 1, b: { c: 2 } }
 
-// // Problem 4: Merging Objects
-// const object2 = { b: 3, c: 4 };
-// const mergedObject = mergeObjects(object1, object2);
-// console.log('Merged Object:', mergedObject); // { a: 1, b: 3, c: 4}
+// Problem 4: Merging Objects
+const object2 = { b: 3, c: 4 };
+const mergedObject = mergeObjects(object1, object2);
+console.log('Merged Object:', mergedObject); // { a: 1, b: 3, c: 4}
---
+++ modules/objectUtils.js
@@ -0,0 +1,11 @@
+const _ = require('lodash');
+
+function deepCloneObject(object1) {
+  return _.cloneDeep(object1);
+}
+
+function mergeObjects(object2, object1) {
+  return _.merge(object2, object1);
+}
+
+module.exports = { deepCloneObject, mergeObjects };
