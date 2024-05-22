5650156be85133f1efc581fc36a594829805bab1
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-05-21 21:34:00 -0500

    added arrayUtils

---
+++ modules/arrayUtils.js
@@ -0,0 +1,11 @@
+const _ = require('lodash');
+
+function chunkArray(array1) {
+  return _.chunk(array1, 2);
+}
+
+function findUniqueElements(array2) {
+  return _.uniq(array2);
+}
+
+module.exports = { chunkArray, findUniqueElements };
